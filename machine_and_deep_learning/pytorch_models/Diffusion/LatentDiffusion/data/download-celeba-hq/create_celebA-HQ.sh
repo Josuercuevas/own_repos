b53bfb60fad007703353279da5fc75985dd4c69b
Josue Cuevas <josuercuevas@gmail.com>
2024-06-06 19:27:36 +0800

    ldm: #added own taming module to avoid loaded celebA-HQ as numpy arrays

---
+++ machine_and_deep_learning/pytorch_models/Diffusion/LatentDiffusion/data/download-celeba-hq/create_celebA-HQ.sh
@@ -0,0 +1,23 @@
+WORKDIR="$( cd "$(dirname "$0")" ; pwd -P )"
+echo $WORKDIR
+
+# Download CelebA unaligned images
+python $WORKDIR/download_celebA.py $1
+
+# Download CelebA-HQ deltas
+python $WORKDIR/download_celebA_HQ.py $1
+
+# Extract CelebA images
+echo "Extracting CelebA images"
+cat $1/celebA/img_celeba.7z.0* > $1/celebA/img_celeba.7z
+7z -mmt=on -mmt=64 x $1/celebA/img_celeba.7z -o$1/celebA
+
+# Create CelebA-HQ images
+echo "Extracting HQ images"
+python $WORKDIR/create_celeba_HQ.py \
+        --h5_filename celeba-hq-1024x1024.h5 \
+        --celeba_dir $1/celebA \
+        --delta_dir $1/celebA-HQ \
+        --output_dir $1 \
+        --num_threads 64 \
+        --num_tasks 500
